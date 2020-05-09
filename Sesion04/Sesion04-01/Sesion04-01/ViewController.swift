033ca2a842661efc7a45aaf625d83ee90004aeaa
Kenyi Rodriguez <krodriguez@solera.pe>
2020-05-09 16:23:51 -0500

    Sesion04-01

--- Sesion04/Sesion04-01/Sesion04-01/ViewController.swift
+++ Sesion04/Sesion04-01/Sesion04-01/ViewController.swift
@@ -10,13 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var scrollContent            : UIScrollView!
+    @IBOutlet weak var bottonConstraintScroll   : NSLayoutConstraint!
+    
+    @IBAction func tapToCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
     }
     
-    func hola() {
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        UIView.animate(withDuration: 0.3) {
+            self.bottonConstraintScroll.constant = keyboardFrame.size.height
+            self.view.layoutIfNeeded()
+        }
+    
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        
+        UIView.animate(withDuration: 0.3) {
+            self.bottonConstraintScroll.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func goToEndScroll() {
+        
+        let heightScroll = self.scrollContent.frame.size.height
+        let heightContent = self.scrollContent.contentSize.height
+        
+        let finalPosY = heightContent - heightScroll
+        let point = CGPoint(x: 0, y: finalPosY)
+        self.scrollContent.setContentOffset(point, animated: true)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
